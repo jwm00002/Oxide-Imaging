b376b7b8f3c39564747ac153c64dad56d7ed63fe
jwm00002 <jwm00002@mix.wvu.edu>
2024-06-04 11:31:54 -0400

    Adding Existing Files to Github
    Created the github repo and added the existing project files

---
+++ notes.m
@@ -0,0 +1,29 @@
+%check preedge and post edge difference and
+%compare first and second maps used for subtraction
+%and see if metal gets smaller in channels
+
+%try running with no NbO
+
+%create a github for this project to keep changes up to date for everybody?
+
+%open and normalize D4 sample data
+%{
+spot2 = XraySpecFunctions.readSpectraFile("D4_spot2_006.r");
+spot2(:,2) = spot2(:,2) - spot2(1,2);
+spot2(:,2) = spot2(:,2)*(1/spot2(303,2));
+spot3 = XraySpecFunctions.readSpectraFile("D4_spot3_007.r");
+spot3(:,2) = spot3(:,2) - spot3(1,2);
+spot3(:,2) = spot3(:,2)*(1/spot3(323,2));
+spot4 = XraySpecFunctions.readSpectraFile("D4_spot4_000.r");
+spot4(:,2) = spot4(:,2) - spot4(1,2);
+spot4(:,2) = spot4(:,2)*(1/spot4(355,2));
+spot5 = XraySpecFunctions.readSpectraFile("D4_spot5_000.r");
+spot5(:,2) = spot5(:,2) - spot5(1,2);
+spot5(:,2) = spot5(:,2)*(1/spot5(353,2));
+spot6 = XraySpecFunctions.readSpectraFile("D4_spot6_000.r");
+spot6(:,2) = spot6(:,2) - spot6(1,2);
+spot6(:,2) = spot6(:,2)*(1/spot6(327,2));
+spot7 = XraySpecFunctions.readSpectraFile("D4_spot7_000.r");
+spot7(:,2) = spot7(:,2) - spot7(1,2);
+spot7(:,2) = spot7(:,2)*(1/spot7(306,2));
+%}
---
+++ shift_optimization.m
@@ -0,0 +1,34 @@
+clc;clear;close all;
+Nb2O5Percent = zeros(1,337);
+NbO2Percent = zeros(1,337);
+NbOPercent = zeros(1,337);
+residualAvg = zeros(1,337);
+dataShift = zeros(1,337);
+NbOShift = zeros(1,337);
+NbO2Shift = zeros(1,337);
+[mapData,mapEnergies] = XraySpecFunctions.loadAllMapData('D4 map data');
+l = 1;
+tic
+%loop through data shifts
+for i = -3.8:-0.1:-4.8
+%loop through each NbO shift
+for j = -1:-0.1:-1.6
+    %lop through each NbO2 shift
+    for k = -0.2:0.1:1
+        disp("Run: "+l)
+        [Nb2O5,NbO2,NbO,residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,i,"NewNbref.txt",0,"Nb2O5 Data.txt",0,"NbO2 Data.txt",k,"Nb2O5 Data.txt",j);
+        Nb2O5Percent(l) = Nb2O5;
+        NbO2Percent(l) = NbO2;
+        NbOPercent(l) = NbO;
+        residualAvg(l) = residual_avg;
+        dataShift(l) = i;
+        NbOShift(l) = j;
+        NbO2Shift(l) = k;
+        l = l+1;
+    end
+end
+end
+toc
+%write table to excel file
+T = table(dataShift',NbOShift',NbO2Shift',Nb2O5Percent',NbO2Percent',NbOPercent',residualAvg');
+writetable(T,'NboptimizationData.xls','WriteRowNames',true);
