4acc060a7e59f4ffdd72330bf1ff0fe325192300
jwm00002 <jwm00002@mix.wvu.edu>
2024-06-05 12:45:17 -0400

    optimization work
    micky_optimization does not work. changed outputs of percent analysis to now include an average of 9 points in the low region. May need more points to get a better average.

--- micky_optimization.m
+++ micky_optimization.m
@@ -1,20 +1,52 @@
 [mapData,mapEnergies] = XraySpecFunctions.loadAllMapData('D4 map data');
 %run 3 data shifts
 %data 3.8 4.0 4.2
-[one.Nb2O5,one.NbO2,one.NbO,one.residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,3.8,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",0,"Nb2O5 Data.txt",0);
-[two.Nb2O5,two.NbO2,two.NbO,two.residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,4.0,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",0,"Nb2O5 Data.txt",0);
-[three.Nb2O5,three.NbO2,three.NbO,three.residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,4.2,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",0,"Nb2O5 Data.txt",0);
-%check which is closest to desired percentages
-index = find(interp1([one.Nb2O5,two.Nb2O5,three.Nb2O5],[one.Nb2O5,two.Nb2O5,three.Nb2O5],0.85));
-if index == 1
-    
-elseif index == 2
-    
-elseif index == 3
-
+mapShift = 0;
+diff = 100;
+for i = -3.8:-0.2:-4.2
+    [Nb2O5,NbO2,NbO,residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,i,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",0,"Nb2O5 Data.txt",0);
+    if abs(0.85-Nb2O5)<abs(diff)
+        mapShift = i;
+        diff = 0.85-Nb2O5;
+    end
 end
+%{
+if diff>0 % difference is positive so Nb2O5 is less than 0.85
+    %high
+    
+    %low
+    
+    %diff/high-low
+    weight = 1-(abs(diff)/(mapShiftHigh-mapShift));
+    mapShift = (weight*mapShift+(1-weight)*mapShiftHigh);
+elseif diff<0 %difference is negative so closest is larger than data
+    %high
+    
+    %low
+
+    weight = 1-(abs(diff)/(mapShift-mapShiftLow));
+    mapShift = (weight*mapShift+(1-weight)*mapShiftLow);
+end
+%}
 %go to next iteration step and repeat for other references
 %NbO -1 -1.3 -1.6
+NbOShift = 0;
+diff = 100;
+for i = -1:-0.3:-1.6
+    [Nb2O5,NbO2,NbO,residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,mapShift,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",0,"Nb2O5 Data.txt",i);
+    if abs(0.85-Nb2O5)<abs(diff)
+        NbOshift = i;
+        diff = 0.85-Nb2O5;
+    end
+end
 %NbO2 -0.2 0.2 1
-
-%85 Nb2O5
+NbO2Shift = 0;
+diff = 100;
+for i = -0.2:0.2:1
+    [Nb2O5,NbO2,NbO,residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,mapShift,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",i,"Nb2O5 Data.txt",NbOshift);
+    if abs(0.85-Nb2O5)<abs(diff)
+        NbO2shift = i;
+        diff = 0.85-Nb2O5;
+    end
+end
+[mapData, mapEnergies] = XraySpecFunctions.xray_mapAnalysis("D4 map data",mapShift,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",NbO2Shift,"Nb2O5 Data.txt",NbOshift);
--- shift_optimization.m
+++ shift_optimization.m
@@ -2,6 +2,7 @@ clc;clear;close all;
 Nb2O5Percent = zeros(1,337);
 NbO2Percent = zeros(1,337);
 NbOPercent = zeros(1,337);
+LowRegion = zeros(1,337);
 residualAvg = zeros(1,337);
 dataShift = zeros(1,337);
 NbOShift = zeros(1,337);
@@ -16,10 +17,11 @@ for j = -1:-0.1:-1.6
     %lop through each NbO2 shift
     for k = -0.2:0.1:1
         disp("Run: "+l)
-        [Nb2O5,NbO2,NbO,residual_avg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,i,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",k,"Nb2O5 Data.txt",j);
+        [Nb2O5,NbO2,NbO,residual_avg,lowRegionAvg] = XraySpecFunctions.xray_percentAnalysis(mapData,mapEnergies,i,"NewNbref.txt",0,"nbsi2.txt",0.25,"Nb2O5 Data.txt",0,"NbO2 Data.txt",k,"Nb2O5 Data.txt",j);
         Nb2O5Percent(l) = Nb2O5;
         NbO2Percent(l) = NbO2;
         NbOPercent(l) = NbO;
+        LowRegion(l) = lowRegionAvg;
         residualAvg(l) = residual_avg;
         dataShift(l) = i;
         NbOShift(l) = j;
@@ -30,5 +32,5 @@ end
 end
 toc
 %write table to excel file
-T = table(dataShift',NbOShift',NbO2Shift',Nb2O5Percent',NbO2Percent',NbOPercent',residualAvg');
-writetable(T,'NboptimizationData.xls','WriteRowNames',true);
+T = table(dataShift',NbOShift',NbO2Shift',Nb2O5Percent',NbO2Percent',NbOPercent',LowRegion',residualAvg');
+writetable(T,'NboptimizationData.xls','WriteRowNames',true,'Sheet','NewNbRef');
